60731a1646373eb2b9434482dd9f518261a2ff35
Camila Mier Cadena <camimiercadena@gmail.com>
2023-01-05 15:15:35 -0500

    CRUD Prueba1

---
+++ Prueba-Crud/resources/views/categorias/show.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">DATOS</div>
+                <!--CONTENIDO DE LA PAGINA -->
+                <label for="">ID -> {{$categoria->id}}</label>
+                <label for="">CODIGO -> {{$categoria->codigo}}</label>
+                <label for="">NOMBRE -> {{$categoria->nombre}}</label>
+                <!-- FIN DE CONTENIDO -->
+
+            </div>
+            <div class="float-right">
+                 <a class="btn btn-primary" href="{{ route('categorias.index') }}"> Regresar</a>
+             </div>
+        </div>
+    </div>
+</div>
+
+
+@endsection
+
---
+++ Prueba-Crud/resources/views/home.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">GESTION DE CATEGORIAS</div>
+
+
+
+                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                  
+
+                <div class="d-grid gap-2 col-6 mx-auto">
+                <a href="{{ route('categorias.index') }}" type="button" class="btn btn-outline-info ">EMPEZAR</a>
+
+                </div>
+                   
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
